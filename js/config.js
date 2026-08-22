6d44b42a100058d3f673bb15b24f267a600e22f0
Aisermelk <30874332+Aisermelk@users.noreply.github.com>
2026-08-22 14:02:52 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    whatsapp: "5599999999999",
-    email: "contato@v8digital.com.br",
-    textos: {
-        ctaWhatsapp: "Olá! Vi o site da V8 Digital e quero saber mais sobre os serviços."
-    }
-};
